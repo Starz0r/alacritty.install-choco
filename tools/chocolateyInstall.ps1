11c1ee0f8737aeb9ee48e0617ebf79dd0e151913
Starz0r <starz0r@starz0r.com>
2019-11-26 14:53:20 -0600

    Bump to version 0.4.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "alacritty.install"
-$url = "https://github.com/jwilm/alacritty/releases/download/v0.3.3/Alacritty-v0.3.3-windows-installer.msi"
-$checksum = "11e4c3703d43564a9aa3d4cdca1ca43c215487c0e137c2e11bc8fbc41a8f5aa298d038901b40407e17940eb2b92e9600a417abba25ce24a46b933f13aeefa375"
+$url = "https://github.com/jwilm/alacritty/releases/download/v0.4.0/Alacritty-v0.4.0-windows-installer.msi"
+$checksum = "2CC9F750C2A4096232F2F8AA9A5622947E70FC09F0A605BCA132801D3E998488CED4544ED69FCFE5C165D01320A94BEF565470851D94DD3FC3B9FCD20A8D66DA"
 
 $ErrorActionPreference = 'Stop'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
