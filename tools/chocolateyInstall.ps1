37006317a2946e191fcf25118ca95d943ae434f1
Starz0r <starz0r@starz0r.com>
2020-01-21 19:49:43 -0600

    Bump to version 0.4.1
    Includes squashed changes from the following commits:
    https://github.com/Starz0r/alacritty-choco/commit/6b1a057f087a1d0c6909dabc88b9ce18bc0f5678
    https://github.com/Starz0r/alacritty-choco/commit/933c7a751c21e2d6cf9da2eaaef43b7540841145
    https://github.com/Starz0r/alacritty-choco/commit/28b6d2930c281e9e0fa8df91f9dc028389845b83
    https://github.com/Starz0r/alacritty-choco/commit/336d8980e0b7003c8fe56ebe51b2226ba1485071
    https://github.com/Starz0r/alacritty-choco/commit/1e9a6330145b30732d330e2db0c23d056332b6ff
    https://github.com/Starz0r/alacritty-choco/commit/c137870192ce6dc742726be5fcdd6f51121d3511
    https://github.com/Starz0r/alacritty-choco/commit/32de9984a1b29acb0b2c87c97251e00ee313830f

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "alacritty.install"
-$url = "https://github.com/jwilm/alacritty/releases/download/v0.4.0/Alacritty-v0.4.0-windows-installer.msi"
-$checksum = "2CC9F750C2A4096232F2F8AA9A5622947E70FC09F0A605BCA132801D3E998488CED4544ED69FCFE5C165D01320A94BEF565470851D94DD3FC3B9FCD20A8D66DA"
+$url = "https://github.com/alacritty/alacritty/releases/download/v0.4.1/Alacritty-v0.4.1-windows-installer.msi"
+$checksum = "C8CA50E614F89CB6C29A28518361E7D090B53BD9BCD7979AF472BC3D588C42F02F8040D38A7F0747A2E0F22209866CE1B492D650846ED1BC9BE902B7A3CDC2F9"
 
 $ErrorActionPreference = 'Stop'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
